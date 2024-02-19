addce83bdb4daaa2812e78e7528ce7f2b4284cd9
Marcus Gonzalez <marcusgonzalez267@outlook.com>
2024-02-19 18:33:31 -0500

    added user authentication

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        user();
         repeat_question();
     }
 
+    public static void user() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("username: ");
+        String answer = scan.nextLine();
+        Scanner scan_2 = new Scanner(System.in);
+        System.out.println("password: ");
+        String answer_2 = scan_2.nextLine();
+
+        if (!Objects.equals(answer, "Washington") || !Objects.equals(answer_2, "1776")) {
+            System.out.println("Incorrect");
+            user();
+        }
+
+    }
     public static void repeat_question() {
         Scanner scan = new Scanner(System.in);
         System.out.println("-calculator \n-math test \n-show list \n-finished \nenter command: ");
